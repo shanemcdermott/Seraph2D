f6095c370afa9ced8538814b11cd17167f1fd29a
Shane <shanemcdermott@unomaha.edu>
2018-02-20 08:39:29 -0600

    Added Smaller branches

---
+++ Seraph2D/Assets/Scripts/TreeBuilder.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeBuilder : MonoBehaviour
+{
+    public Vector2 size;
+
+	// Use this for initialization
+	void Start ()
+    {
+        //AddBranch();	
+	}
+	
+	void AddBranch()
+    {
+        GameObject branch = GameObject.Instantiate(gameObject, gameObject.transform);
+        branch.transform.Rotate(Vector3.forward, 45);
+    }
+}
